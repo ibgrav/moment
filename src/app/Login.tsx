b41a9192e27889c34698a9c7e70e609b7941522d
Isaac Graves <ibgrav@gmail.com>
2023-01-15 10:48:40 -0800

    add github

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -1,9 +1,34 @@
+import { Provider } from "@supabase/supabase-js";
 import { signInWithProvider } from "../lib/supabase/auth";
+
+interface ProviderButton {
+  label: string;
+  provider: Provider;
+}
+
+const buttons: ProviderButton[] = [
+  {
+    label: "Google",
+    provider: "google",
+  },
+  {
+    label: "GitHub",
+    provider: "github",
+  },
+];
 
 export function Login() {
   return (
-    <main>
-      <button onClick={() => signInWithProvider("google")}>Sign in with Google</button>
+    <main className="flex flex-col gap-4 py-4 items-center justify-center">
+      {buttons.map(({ label, provider }) => (
+        <button
+          key={provider}
+          onClick={() => signInWithProvider(provider)}
+          className="border-2 py-2 px-4 rounded-sm hover:shadow-sm"
+        >
+          Sign in with {label}
+        </button>
+      ))}
     </main>
   );
 }
